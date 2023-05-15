c2829572c2abbc2224771e08f9e73cfefe02b42e
Hugo Gomes <hugo7gomes@gmail.com>
2023-05-15 16:24:25 +0100

    Average workblock and standrd deviation

--- app/data_analysis/averageWorkBlock.py
+++ app/data_analysis/averageWorkBlock.py
@@ -1,29 +1,19 @@
 import requests
 from config import *
+import statistics
 
 def getAverageWorkBlock(id):
     workBlocks = requests.get(backEndIP + '/schedules/' + str(id) + '/workBlocks')
     workBlocksHour = dict()
     if(workBlocks.status_code != 200):
         return -1
-    
+    countTime = []
     for workBlockId in workBlocks.json()["workBlockIds"]:
         workBlock = requests.get(backEndIP + '/workBlocks/' + str(workBlockId)) 
-        hour = workBlock.json()["workBlockInfo"]["start_time"] // 3600
-        if(hour in workBlocksHour):
-            workBlocksHour[hour] += 1
-        else:
-            workBlocksHour[hour] = 1
-    result = []
-    for i in range(24):
-        workBlock = dict()
-        workBlock['ti'] = i * 3600
-        workBlock['tf'] = (i + 1) * 3600
-        if(i not in workBlocksHour):
-            workBlock['value'] = 0
-        else:
-            workBlock['value'] = workBlocksHour[i]
-
-        result.append(workBlock)
-
-    return {'schedule': id, 'data': result}
+        startTime = workBlock.json()["workBlockInfo"]["start_time"]
+        endTime = workBlock.json()["workBlockInfo"]["end_time"]
+        countTime.append(endTime - startTime)
+    
+    average = statistics.mean(countTime)
+    standardDeviation = statistics.stdev(countTime)
+    return {'schedule': id, 'average': average, 'standardDeviation': standardDeviation}
